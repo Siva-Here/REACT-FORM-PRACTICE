b10b58859c29d366275957497889c5391af431cf
siva <sivahere9484@gmail.com>
2024-05-11 19:21:18 +0530

    forms

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React,{useState} from "react";
 function App() {
+  const [data,setData]=useState({
+    username:'',
+    password:''
+  })
+  const {username,password}=data;
+  const changeHandler=(e)=>{
+    setData({...data,[e.target.name]:e.target.value});
+  }
+
+  const submitHandler=(e)=>{
+    e.preventDefault();
+    console.log(data);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <center>
+      <div className="form">
+     <form onSubmit={submitHandler}>
+       <div className="input-container">
+         <label>Username </label>
+         <input type="text" name="username" required value={username} onChange={changeHandler}/>
+       </div>
+       <div className="input-container">
+         <label>Password </label>
+         <input type="password" name="password" required value={password} onChange={changeHandler}/>
+       </div>
+       <div className="button-container">
+         <input type="submit" name="submit"/>
+       </div>
+     </form>
+   </div>
+   </center>
     </div>
   );
 }
